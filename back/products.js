6266777577e634b984e7f8858a1601c53b33d44b
OunifiSamir <59454272+OunifiSamirIT@users.noreply.github.com>
2023-03-06 13:48:59 +0100

    payy

---
+++ back/products.js
@@ -0,0 +1,32 @@
+
+const products = [
+  {
+    id: 1,
+    name: "Events Tunisie",
+    brand: "Rap",
+    desc: "12/10/2022",
+    price: 999,
+    image:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.parkgrandkensington.co.uk%2Fblog%2Fwp-content%2Fuploads%2F2020%2F02%2Fmusic-and-art-fest-california.jpg&imgrefurl=https%3A%2F%2Fwww.parkgrandkensington.co.uk%2Fblog%2Fmusic-events-london-summer%2F&tbnid=Mq4sszJsOJoV6M&vet=12ahUKEwiJ-b2Tocf9AhV157sIHfEHA6UQMygEegUIARDCAQ..i&docid=KCY9-IWK0QNAVM&w=1000&h=667&q=music%20events&ved=2ahUKEwiJ-b2Tocf9AhV157sIHfEHA6UQMygEegUIARDCAQ",
+  },
+  {
+    id: 2,
+    name: "Freestyle Stream",
+    brand: "//http/www.stremlive.com",
+    desc: "05/12/2001",
+    price: 699,
+    image:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.parkgrandkensington.co.uk%2Fblog%2Fwp-content%2Fuploads%2F2020%2F02%2Fmusic-and-art-fest-california.jpg&imgrefurl=https%3A%2F%2Fwww.parkgrandkensington.co.uk%2Fblog%2Fmusic-events-london-summer%2F&tbnid=Mq4sszJsOJoV6M&vet=12ahUKEwiJ-b2Tocf9AhV157sIHfEHA6UQMygEegUIARDCAQ..i&docid=KCY9-IWK0QNAVM&w=1000&h=667&q=music%20events&ved=2ahUKEwiJ-b2Tocf9AhV157sIHfEHA6UQMygEegUIARDCAQ",
+  },
+  {
+    id: 3,
+    name: "Drill",
+    brand: "Strem challange",
+    desc: "2/07/2023",
+    price: 399,
+    image:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.parkgrandkensington.co.uk%2Fblog%2Fwp-content%2Fuploads%2F2020%2F02%2Fmusic-and-art-fest-california.jpg&imgrefurl=https%3A%2F%2Fwww.parkgrandkensington.co.uk%2Fblog%2Fmusic-events-london-summer%2F&tbnid=Mq4sszJsOJoV6M&vet=12ahUKEwiJ-b2Tocf9AhV157sIHfEHA6UQMygEegUIARDCAQ..i&docid=KCY9-IWK0QNAVM&w=1000&h=667&q=music%20events&ved=2ahUKEwiJ-b2Tocf9AhV157sIHfEHA6UQMygEegUIARDCAQ",
+  },
+];
+
+module.exports = products;
